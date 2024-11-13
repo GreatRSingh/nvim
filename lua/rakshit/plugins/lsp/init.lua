a1c1e7c7c2ae745a22d5fd2b5898339e559b12fa
GreatRSingh <rakshitsingh421@gmail.com>
2024-11-13 22:23:05 +0530

    old updates

--- lua/rakshit/plugins/lsp/init.lua
+++ lua/rakshit/plugins/lsp/init.lua
@@ -1,6 +1,6 @@
 require("mason").setup()
 require("mason-lspconfig").setup({
-    ensure_installed = { "lua_ls" }
+    ensure_installed = { "lua_ls", "emmet_ls" }
 })
 
 local on_attach = function(_, _)
@@ -24,3 +24,20 @@ require("lspconfig").lua_ls.setup {
   },
 }
 
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+require("lspconfig").emmet_ls.setup({
+    on_attach = on_attach,
+    capabilities = capabilities,
+    filetypes = { "css", "eruby", "html", "javascript", "javascriptreact", "less", "sass", "scss", "svelte", "pug", "typescriptreact", "vue" },
+    init_options = {
+      html = {
+        options = {
+          -- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
+          ["bem.enabled"] = true,
+        },
+      },
+    }
+})
